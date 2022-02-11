a6c1eefa27f3abd2552eef3bfae988705d454e90
Laura Edith López Fregoso <laura.lopez204820@potros.itson.edu.mx>
2022-02-11 11:32:38 -0700

    create authentication service and login UI

--- src/JournalApp.js
+++ src/JournalApp.js
@@ -3,9 +3,11 @@ import React, { useEffect, useState } from 'react'
 import Amplify, { API, graphqlOperation } from 'aws-amplify'
 import { createTodo } from './graphql/mutations'
 import { listTodos } from './graphql/queries'
-
+import { withAuthenticator, Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css';
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
+
 
 const initialState = { name: '', description: '' }
 
@@ -42,31 +44,64 @@ export const JournalApp = () => {
   }
 
   return (
-    <div style={styles.container}>
-      <h2>Amplify Todos</h2>
-      <input
-        onChange={event => setInput('name', event.target.value)}
-        style={styles.input}
-        value={formState.name}
-        placeholder="Name"
-      />
-      <input
-        onChange={event => setInput('description', event.target.value)}
-        style={styles.input}
-        value={formState.description}
-        placeholder="Description"
-      />
-      <button style={styles.button} onClick={addTodo}>Create Todo</button>
-      {
-        todos.map((todo, index) => (
-          <div key={todo.id ? todo.id : index} style={styles.todo}>
-            <p style={styles.todoName}>{todo.name}</p>
-            <p style={styles.todoDescription}>{todo.description}</p>
-          </div>
-        ))
-      }
-    </div>
-  )
+    <Authenticator>
+      {({ signOut, user }) => (
+        <div style={styles.container}>
+          <h1>Hello {user.username}</h1>
+          <button style={styles.button} onClick={signOut}>Sign out</button>
+          <br />
+          <h2>Amplify Todos</h2>
+          <input
+            onChange={event => setInput('name', event.target.value)}
+            style={styles.input}
+            value={formState.name}
+            placeholder="Name"
+          />
+          <input
+            onChange={event => setInput('description', event.target.value)}
+            style={styles.input}
+            value={formState.description}
+            placeholder="Description"
+          />
+          <button style={styles.button} onClick={addTodo}>Create Todo</button>
+          {
+            todos.map((todo, index) => (
+              <div key={todo.id ? todo.id : index} style={styles.todo}>
+                <p style={styles.todoName}>{todo.name}</p>
+                <p style={styles.todoDescription}>{todo.description}</p>
+              </div>
+            ))
+          }
+        </div>
+      )}
+    </Authenticator>
+  );
+//   return (
+//     <div style={styles.container}>
+//       <h2>Amplify Todos</h2>
+//       <input
+//         onChange={event => setInput('name', event.target.value)}
+//         style={styles.input}
+//         value={formState.name}
+//         placeholder="Name"
+//       />
+//       <input
+//         onChange={event => setInput('description', event.target.value)}
+//         style={styles.input}
+//         value={formState.description}
+//         placeholder="Description"
+//       />
+//       <button style={styles.button} onClick={addTodo}>Create Todo</button>
+//       {
+//         todos.map((todo, index) => (
+//           <div key={todo.id ? todo.id : index} style={styles.todo}>
+//             <p style={styles.todoName}>{todo.name}</p>
+//             <p style={styles.todoDescription}>{todo.description}</p>
+//           </div>
+//         ))
+//       }
+//     </div>
+//   )
 }
 
 const styles = {
@@ -77,3 +112,6 @@ const styles = {
   todoDescription: { marginBottom: 0 },
   button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
 }
+
+
+export default withAuthenticator(JournalApp)
